067482166fa7781d86c9d7345caf44b7fbb538fd
WhiteLady1 <megan15963@gmail.com>
2020-11-01 11:38:38 +0100

    Orederind finished

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 
 console.log('funguju!');
 
+//----------- NAVIGACION -------------
 const btnElm = document.querySelector('#nav-btn');
 btnElm.addEventListener('click', () => {
   const navElm = document.querySelector('nav');
@@ -17,3 +18,19 @@ for (let i = 0; i < navClose.length; i++) {
     navigace.classList.toggle('nav-closed');
   });
 }
+
+//----------- ORDERING -------------
+let ordered = false;
+const btnOrder = document.querySelector('.order-btn');
+btnOrder.addEventListener('click', () => {
+  const elementDrinkCup = document.querySelector('.drink__cup');
+  if (ordered === false) {
+    elementDrinkCup.classList.toggle('.drink__cup--selected');
+    btnOrder.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    elementDrinkCup.classList.toggle('.drink__cup--selected');
+    btnOrder.textContent = 'Objednat';
+    ordered = false;
+  }
+});
