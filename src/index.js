4cf8accf5f5c62a26929c48824830afcfd06ab8e
WhiteLady1 <megan15963@gmail.com>
2020-11-01 11:23:25 +0100

    Navigace komplet HURÁ

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const btnElm = document.querySelector('#nav-btn');
+btnElm.addEventListener('click', () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+});
+
+const navClose = document.querySelectorAll('a');
+console.log(navClose);
+for (let i = 0; i < navClose.length; i++) {
+  navClose[i].addEventListener('click', () => {
+    const navigace = document.querySelector('nav');
+    navigace.classList.toggle('nav-closed');
+  });
+}
