984db36cdeac4a162db9610dbf962bcd22d985d5
WhiteLady1 <megan15963@gmail.com>
2020-11-01 15:30:00 +0100

    Komponeneta Drink - problem s order

--- src/index.js
+++ src/index.js
@@ -21,19 +21,22 @@ for (let i = 0; i < navClose.length; i++) {
 
 //----------- ORDERING -------------
 let ordered = false;
-const btnOrder = document.querySelector('.order-btn');
-btnOrder.addEventListener('click', () => {
-  const elementDrinkCup = document.querySelector('.drink__cup');
-  if (ordered === false) {
-    elementDrinkCup.classList.toggle('.drink__cup--selected');
-    btnOrder.textContent = 'Zrušit';
-    ordered = true;
-  } else {
-    elementDrinkCup.classList.toggle('.drink__cup--selected');
-    btnOrder.textContent = 'Objednat';
-    ordered = false;
-  }
-});
+const btnOrder = document.querySelectorAll('.order-btn');
+for (let i = 0; i < btnOrder.length; i++) {
+  btnOrder[i].addEventListener('click', () => {
+    const elementDrinkCup = document.querySelector('.drink__cup');
+    if (ordered === false) {
+      elementDrinkCup.classList.toggle('.drink__cup--selected');
+      btnOrder[i].textContent = 'Zrušit';
+      ordered = true;
+    } else {
+      elementDrinkCup.classList.toggle('.drink__cup--selected');
+      btnOrder[i].textContent = 'Objednat';
+      ordered = false;
+    }
+  });
+}
+console.log(btnOrder.length);
 
 //----------- Ingredience jako komponenta ----------
 const pole = {
@@ -44,20 +47,6 @@ const pole = {
 const prvniVrstva = document.querySelectorAll('.layer');
 
 //----------- Seznam ingrediencí -----------
-const layers = [
-  {
-    color: '#feeeca',
-    label: 'mléčná pěna--',
-  },
-  {
-    color: '#fed7b0',
-    label: 'teplé mléko+-',
-  },
-  {
-    color: '#613916',
-    label: 'espresso++',
-  },
-];
 
 const Layers = (props) => {
   let result = '<div class="drink__layers">';
@@ -84,6 +73,7 @@ const romano = {
     },
   ],
 };
+console.log(romano.ordered);
 
 const Drink = (props) => {
   const newDrink = document.createElement('div');
